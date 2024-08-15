b68ccf6d51143f326e94d72def54663368248a17
achraf-sehlaoui-alx <sehlaoui.achraf@gmail.com>
2024-08-15 17:02:27 +0000

    i'm back, fixing 00

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,10 +11,7 @@ def number_of_subscribers(subreddit):
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
     if response.status_code == 404:
         return 0
-    
     results = response.json().get("data")
-    
     return results.get("subscribers")
