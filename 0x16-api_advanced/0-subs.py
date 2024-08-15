9d0171bdd662f2c4716b1b6157e7043f332be863
achraf-sehlaoui-alx <sehlaoui.achraf@gmail.com>
2024-08-15 17:52:12 +0000

    t0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 """
-Script that queries subscribers on a given Reddit subreddit.
+    function Reddit API and returns the number of subscribers
 """
-
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
+    """
+    Return the total number of subscribers on a given subreddit
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "request"}
     response = requests.get(url, headers=headers, allow_redirects=False)
+    
     if response.status_code == 200:
         data = response.json()
         subscribers = data['data']['subscribers']
